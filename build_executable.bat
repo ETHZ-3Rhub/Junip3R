ad4889da157f433a340d6938ef8c4a5745a46553
Marcel Schmutz <marcel.schmutz@hest.ethz.ch>
2026-07-08 14:16:19 +0200

    Added pyinstaller build script

---
+++ build_executable.bat
@@ -0,0 +1,8 @@
+:: The pyinstaller command is provided by the pyinstaller Python module, so the command needs to be executed inside a valid Python environment
+pyinstaller^
+ --add-data "src/junip3r/res/*;junip3r/app/res"^
+ --onedir^
+ --name "Junip3R"^
+ --icon "src/junip3r/res/junip3r_icon.ico"^
+ --noconsole^
+ src/junip3r/main.py -y
