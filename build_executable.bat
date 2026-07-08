72aee10102f4adf992720540729c804968cf9958
Marcel Schmutz <marcel.schmutz@hest.ethz.ch>
2026-07-08 15:39:13 +0200

    Added compliance for PySide6 license

--- build_executable.bat
+++ build_executable.bat
@@ -1,6 +1,8 @@
 :: The pyinstaller command is provided by the pyinstaller Python module, so the command needs to be executed inside a valid Python environment
 pyinstaller^
  --add-data "src/junip3r/res/*;junip3r/app/res"^
+ --add-data "LICENSE;."^
+ --add-data "licenses/*;licenses"^
  --onedir^
  --name "Junip3R"^
  --icon "src/junip3r/res/junip3r_icon.ico"^
