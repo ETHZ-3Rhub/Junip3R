19b46ea2e4c311a6a3f3cc80fb160ddc2a674835
Marcel Schmutz <marcel.schmutz@hest.ethz.ch>
2026-07-09 13:04:59 +0200

    Fixed ui compilation paths

--- compile_ui.bat
+++ compile_ui.bat
@@ -1,10 +1,9 @@
 :: The pyside6-uic command is provided by the pyside6 Python module, so the command needs to be executed inside the conda environment
-call conda activate Labell3R
 echo Compiling Widgets...
-pyside6-uic "ui/PoseEditor.ui" -o "app/labeller/layout/pose_editor.py"
-pyside6-uic "ui/SelectionControls.ui" -o "app/labeller/layout/selection_controls.py"
-pyside6-uic "ui/ImageNavigation.ui" -o "app/labeller/layout/image_navigation.py"
-pyside6-uic "ui/Editor.ui" -o "app/labeller/layout/editor.py"
+pyside6-uic "ui/PoseEditor.ui" -o "src/junip3r/labeller/layout/pose_editor.py"
+pyside6-uic "ui/SelectionControls.ui" -o "src/junip3r/labeller/layout/selection_controls.py"
+pyside6-uic "ui/ImageNavigation.ui" -o "src/junip3r/labeller/layout/image_navigation.py"
+pyside6-uic "ui/Editor.ui" -o "src/junip3r/labeller/layout/editor.py"
 echo Compiling Main Windows...
-pyside6-uic "ui/Labeller.ui" -o "app/labeller/layout/labeller.py"
-pyside6-uic "ui/FrameExtractor.ui" -o "app/frame_extractor/layout/frame_extractor.py"
+pyside6-uic "ui/Labeller.ui" -o "src/junip3r/labeller/layout/labeller.py"
+pyside6-uic "ui/FrameExtractor.ui" -o "src/junip3r/frame_extractor/layout/frame_extractor.py"
