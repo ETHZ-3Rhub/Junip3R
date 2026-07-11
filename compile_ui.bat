ffad03c80ff9398daac98355da668982d2147bf6
Marcel Schmutz <marcel.schmutz@hest.ethz.ch>
2026-07-11 18:49:18 +0200

    fixup! Restructured editor, removed designer layout

--- compile_ui.bat
+++ compile_ui.bat
@@ -1,6 +1,5 @@
 :: The pyside6-uic command is provided by the pyside6 Python module, so the command needs to be executed inside the conda environment
 echo Compiling Widgets...
-pyside6-uic "ui/PoseEditor.ui" -o "src/junip3r/labeller/layout/pose_editor.py"
 pyside6-uic "ui/SelectionControls.ui" -o "src/junip3r/labeller/layout/selection_controls.py"
 pyside6-uic "ui/ImageNavigation.ui" -o "src/junip3r/labeller/layout/image_navigation.py"
 pyside6-uic "ui/Editor.ui" -o "src/junip3r/labeller/layout/editor.py"
