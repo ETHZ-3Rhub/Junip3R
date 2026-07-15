c4ec8e3d1d1ac052e572e7e10ad1efa798b18983
Marcel Schmutz <marcel.schmutz@hest.ethz.ch>
2026-07-15 09:34:17 +0200

    fixup! Another big model restructuring

--- compile_ui.bat
+++ compile_ui.bat
@@ -1,8 +1,5 @@
 :: The pyside6-uic command is provided by the pyside6 Python module, so the command needs to be executed inside the conda environment
 echo Compiling Widgets...
-pyside6-uic "ui/SelectionControls.ui" -o "src/junip3r/labeller/layout/selection_controls.py"
 pyside6-uic "ui/ImageNavigation.ui" -o "src/junip3r/labeller/layout/image_navigation.py"
-pyside6-uic "ui/Editor.ui" -o "src/junip3r/labeller/layout/editor.py"
 echo Compiling Main Windows...
-pyside6-uic "ui/Labeller.ui" -o "src/junip3r/labeller/layout/labeller.py"
 pyside6-uic "ui/FrameExtractor.ui" -o "src/junip3r/frame_extractor/layout/frame_extractor.py"
